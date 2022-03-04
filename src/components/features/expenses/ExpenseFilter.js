db96971bda956941731376276eec311a62400e27
sarahwimark <sarahwimark@gmail.com>
2022-03-04 16:50:45 +0100

    Working on filter

--- src/components/features/expenses/ExpenseFilter.js
+++ src/components/features/expenses/ExpenseFilter.js
@@ -2,12 +2,16 @@ import React from "react";
 
 import "./ExpenseFilter.css";
 
-export const ExpenseFilter = () => {
+export const ExpenseFilter = ({ onYearChange, value }) => {
+  const onFilterChange = (event) => {
+    onYearChange(event.target.value);
+  };
+
   return (
     <div className="expense-filter">
       <div className="expense-filter__control">
         <label>Filter by year</label>
-        <select>
+        <select value={value} onChange={onFilterChange}>
           <option value="2022">2022</option>
           <option value="2021">2021</option>
           <option value="2020">2020</option>
--- src/components/features/expenses/Expenses.js
+++ src/components/features/expenses/Expenses.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { ExpenseItem } from "./ExpenseItem";
 import { Card } from "../../UI/Card";
 import { ExpenseFilter } from "./ExpenseFilter";
@@ -6,10 +6,16 @@ import { ExpenseFilter } from "./ExpenseFilter";
 import "./Expenses.css";
 
 export const Expenses = ({ items }) => {
+  const [year, setYear] = useState(2020);
+
+  const onYearChange = (year) => {
+    setYear(year);
+  };
+
   return (
     <>
-      <ExpenseFilter />
       <Card className="expenses">
+        <ExpenseFilter value={year} onYearChange={onYearChange} />
         {items.map((expense) => (
           <ExpenseItem
             title={expense.title}
