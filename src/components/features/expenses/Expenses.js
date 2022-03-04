a3d4fe451b08cb185c2e3a86530d15ce53dd2382
sarahwimark <sarahwimark@gmail.com>
2022-03-04 16:35:08 +0100

    Adding a filter by year for the expenses.

--- src/components/features/expenses/Expenses.js
+++ src/components/features/expenses/Expenses.js
@@ -1,19 +1,24 @@
 import React from "react";
 import { ExpenseItem } from "./ExpenseItem";
 import { Card } from "../../UI/Card";
+import { ExpenseFilter } from "./ExpenseFilter";
+
 import "./Expenses.css";
 
 export const Expenses = ({ items }) => {
   return (
-    <Card className="expenses">
-      {items.map((expense) => (
-        <ExpenseItem
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-          key={expense.id}
-        />
-      ))}
-    </Card>
+    <>
+      <ExpenseFilter />
+      <Card className="expenses">
+        {items.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+            key={expense.id}
+          />
+        ))}
+      </Card>
+    </>
   );
 };
