df6557a5e58a8265f6576c7d7a774e14a6811e55
sarahwimark <sarahwimark@gmail.com>
2022-03-07 17:17:35 +0100

    Finishing chart

--- src/components/chart/Chart.js
+++ src/components/chart/Chart.js
@@ -3,13 +3,16 @@ import "./Chart.css";
 import { ChartBar } from "./ChartBar";
 
 export const Chart = ({ dataPoints }) => {
+  const dataPointValues = dataPoints.map((datapoint) => datapoint.value);
+  const maxValue = Math.max(...dataPointValues);
+
   return (
     <div className="chart">
       {dataPoints.map((datapoint) => (
         <ChartBar
           key={datapoint.label}
           value={datapoint.value}
-          maxValue={null}
+          maxValue={maxValue}
           label={datapoint.label}
         />
       ))}
--- src/expenses/features/Expenses.js
+++ src/expenses/features/Expenses.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { ExpensesList } from "../components/ExpensesList";
 import { Card } from "../../components/UI/Card";
 import { ExpenseFilter } from "../components/ExpenseFilter";
+import { ExpensesChart } from "./ExpensesChart";
 import "./Expenses.css";
 
 export const Expenses = ({ expenses }) => {
@@ -19,6 +20,7 @@ export const Expenses = ({ expenses }) => {
     <>
       <Card className="expenses">
         <ExpenseFilter value={year} onYearChange={onYearChange} />
+        <ExpensesChart expenses={filteredExpenses} />
         <ExpensesList expenses={filteredExpenses} />
       </Card>
     </>
